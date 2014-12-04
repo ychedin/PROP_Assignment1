af3f9225eab45c760cbb41efd1d01a2b61930676
Tyfoid00 <mail@oscarwiberg.com>
2014-12-04 15:50:40 +0100

    changed classBased.js. it works, but has protection against cyclic depedancy

---
+++ classBased.js
@@ -0,0 +1,81 @@
+"use strict";
+function createClass(className, superClassList) {
+    var myClass = {
+        new : function () {
+            var object = {
+                call: function (funcName, parameters) {
+                    var dfsResult = this.class.dfs(funcName, parameters);
+                    if (typeof dfsResult === "function") {
+                        return dfsResult(parameters); // actually calls the function.
+                    } else {
+                        throw "'" + funcName + "' is not defined for the calling class.";
+                    }
+                } // end of call();
+            };
+
+            Object.defineProperty(object, 'class', {
+                configurable: false,
+                writable: false,
+                value: myClass
+            });
+
+            return object;
+        }, // end of new();
+
+        dfs: function (funcName, args) { // helper method. Not intended to be called directly.
+            var fn = this[funcName];
+            if (typeof fn === "function") {
+                return fn; // Success! Stop searching.
+            } else if (this.superClassList) { // if this has a list. 
+                for (var i = 0; i < this.superClassList.length; i++) { // go through the list.
+                    fn = this.superClassList[i].dfs(funcName, args);
+                    if (typeof fn === "function") {
+                        return fn; // Success! Stop searching.
+                    }
+                }
+            }
+        } // end of dfs()
+    };
+
+    // do cycle check here!
+
+    Object.defineProperty(myClass, 'className', {
+        configurable: false,
+        writable: false,
+        value: className
+    });
+
+    Object.defineProperty(myClass, 'superClassList', {
+        configurable: false,
+        writable: false,
+        value: superClassList
+    });
+    return myClass;
+}// end of createClass
+
+
+// tests
+
+function peterTest() {
+    document.write("<br>peterTest: ");
+    var class0 = createClass("Class0", null);
+    class0.func = function (arg) {
+        return "func0: " + arg;
+    };
+    var class1 = createClass("Class1", [class0]);
+    var class2 = createClass("Class2", []);
+    class2.func = function (arg) {
+        return "func2: " + arg;
+    };
+    var class3 = createClass("Class3", [class1, class2]);
+    var obj3 = class3.new();
+    var result = obj3.call("func", ["hello"]);
+//where 'result' is assigned "func0: hello".
+ if (result === "func0: hello"){
+     document.write("Success!<br>");
+ }
+    
+
+}
+
+peterTest();
